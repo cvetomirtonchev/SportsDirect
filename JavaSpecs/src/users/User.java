846ca7191325441d38cd2e4960a0e9833084b5b8
Vasil Goranov <vasil.goranov@gmail.com>
2017-03-01 18:13:13 +0200

    added a method that checks the email input and created class Store and comparator for the users Set

--- JavaSpecs/src/users/User.java
+++ JavaSpecs/src/users/User.java
@@ -1,5 +1,7 @@
 package users;
 
+import store.Store;
+
 public class User {
 	
 	private static int idNumber=0;
@@ -7,16 +9,49 @@ public class User {
 	private String password;
 	private String repeatPassword;
 	private String email;
+	private int id;
+	private Store store;
 	
 	
 	
-	public User(String username, String password,String repeatPassword, String email) {
-		super();
-		this.username = username;
-		this.password = password;
+	public User(String username, String password,String repeatPassword, String email, Store store) {
 		
-		this.email = email;
+		// call the method to check if the specified mail already exists:
+		if (emailAlreadyTaken(email)) {
+			System.out.println("This e-mail is already in the database. Please try with a different e-mail. ");
+		}
+		else {
+			this.email = email;
+			this.username = username;
+			this.password = password;
+			this.email = email;
+			this.store = store;
+			this.id = idNumber;
+			idNumber++;
+		}
 	}
 	
+	// checks if an e-mail is already taken
+	private boolean emailAlreadyTaken (String mail) {
+		boolean isTaken = false;
+		for (User u : this.store.getUsers()) {
+			if (mail.equals(u.getEmail())) {
+				isTaken = true;
+				break;
+			}
+		}
+		return isTaken;		
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	
+
 	
 }
